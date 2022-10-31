721cecb603345b7cb91ba0eda469957112db94bc
Syed shihab <108602097+SyedemonShihab@users.noreply.github.com>
2022-10-31 13:15:27 +0600

    new design with days month and year

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
-function clock () {
-let hours = document.getElementById('hour');
-let munites = document.getElementById('munite');
-let seconds = document.getElementById('second');
-let priods = document.getElementById('priod');
+function clock() {
+  let days = document.getElementById('day');
+  let months = document.getElementById('month');
+  let years = document.getElementById('year');
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
-let ampm = h >= 12 ? "pm" : "am" ;
+  let hours = document.getElementById('hour');
+  let minuts = document.getElementById('minute');
+  let seconds = document.getElementById('second');
+  let priods = document.getElementById('priod');
 
+  let d = new Date();
+  let mo = new Date();
+  let y = new Date();
+
+  let h = new Date().getHours();
+  let mi = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  let ampm = (h >= 12) ? "pm" : "am";
+
+ 
+
+  if(h > 12 ){
+    h = h-12;
+  }
+
+  h = (h < 10) ? "0" + h : h;
+  mi = (mi < 10) ? "0" + mi : mi;
+  s = (s < 10) ? "0" + s : s;
+  const day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const today = day[d.getDay()];
+  days.innerHTML = today;
+
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const runningMonth = month[mo.getMonth()];
+  months.innerHTML = runningMonth;
+
+  years.innerHTML = y.getFullYear() 
+
+
+hours.innerHTML = h;
+minuts.innerHTML= mi ;
+seconds.innerHTML = s;
+priods.innerHTML = ampm;
 }
+setInterval(clock,1000);
